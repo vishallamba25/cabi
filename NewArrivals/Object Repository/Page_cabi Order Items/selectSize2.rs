6f03e23cc624e37d61347b187731ee47139d19ad
Vishal <vishal.lamba@neosofttech.com>
2019-05-29 20:49:12 +0530

    no message

---
+++ NewArrivals/Object Repository/Page_cabi Order Items/selectSize2.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>selectSize2</name>
+   <tag></tag>
+   <elementGuidId>89f5874b-e38f-408d-94b2-d0bdef647b19</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//select[@id='sizeSelect_1']/option[@value='L']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//select[@id='sizeSelect_1']/option[@value='L']</value>
+   </webElementProperties>
+</WebElementEntity>

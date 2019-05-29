d30ee32f2f08ebca2c9546c9b8e01886c6a8321d
sushantshinde <sushant.shinde@neosofttech.com>
2019-05-29 14:50:56 +0530

    dynamic object creation (passing dynammic xpath) for rsOrderHistoryConfirm

--- NewArrivals/Scripts/editOrder/rsOrderHistoryConfirm/Script1559044226727.groovy
+++ NewArrivals/Scripts/editOrder/rsOrderHistoryConfirm/Script1559044226727.groovy
@@ -2,15 +2,27 @@ import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
 import org.openqa.selenium.WebElement
 
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObject
 import com.kms.katalon.core.webui.common.WebUiCommonHelper
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
+import commonUtility.UtilityMethods
 import internal.GlobalVariable as GlobalVariable
 
 
 orderID = GlobalVariable.retailOrderID
-orderID= "qwwqeqe"
 println orderID
+//orderID = "1234"
+
+////////////////span[contains(text(), '${orderID}')]/parent::div/parent::div/div[@class='table-row-edit-order']
+String dynamicId = 'Katalon123'
+String xpath = UtilityMethods.concat("//span[contains(text(), '", orderID, "')]/parent::div/parent::div/div[@class='table-row-edit-order']")
+//String xpath = '//div[@id="' + dynamicId + '"]'
+TestObject to = new TestObject("a_link_dynamic")
+to.addProperty("xpath", ConditionType.EQUALS, xpath)
+//WebUI.click(to)
+//////////////
 
 WebUI.click(findTestObject('Object Repository/ReplicatedSite/Page_Trendy Womens Fashion and Outfits  Cabi Clothing/i_Michigan_fa fa-caret-down'))
 
@@ -18,14 +30,14 @@ WebUI.click(findTestObject('Object Repository/ReplicatedSite/Page_Trendy Womens 
 
 WebUI.click(findTestObject('Object Repository/ReplicatedSite/Page_Profile - Cabi Fall 2018 Collection/span_Order History'))
 
-List<WebElement> editButton = WebUiCommonHelper.findWebElements(findTestObject('Object Repository/ReplicatedSite/Page_Profile - Cabi Fall 2018 Collection/a_edit_order'),
-	1)
+WebUI.delay(5)
+
+List<WebElement> editButton = WebUiCommonHelper.findWebElements(to, 1)
 
 assert editButton.size() > 0
 
-if (editButton.size() == 0) {
-	WebUI.click(findTestObject('Object Repository/ReplicatedSite/Page_Profile - Cabi Fall 2018 Collection/a_edit_order'))
+if (editButton.size() > 0) {
+	WebUI.click(to)
 }
-
 
 

291efd6db6ec1a519064371c99fbc69c22154583
Vishal <vishal.lamba@neosofttech.com>
2019-06-11 20:29:54 +0530

    no message

--- NewArrivals/Scripts/virtualShowRSVP/micrositeRSVPValidUser/Script1553241488977.groovy
+++ NewArrivals/Scripts/virtualShowRSVP/micrositeRSVPValidUser/Script1553241488977.groovy
@@ -6,10 +6,7 @@ import com.kms.katalon.core.webui.common.WebUiCommonHelper as WebUiCommonHelper
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 import internal.GlobalVariable as GlobalVariable
-<<<<<<< HEAD
 
-=======
->>>>>>> 57806eadc084f8b7d9029c1ee738c7bd56fbfaf4
 import com.kms.katalon.core.webui.common.WebUiCommonHelper as WebUiCommonHelper
 
 WebUI.openBrowser('')

2e1387cf95a46ed1dddb458519b68f34f1c2d39c
sushantshinde <sushant.shinde@neosofttech.com>
2019-06-13 17:05:26 +0530

    no message

--- NewArrivals/Libs/TempTestCase1560423819123.groovy
+++ NewArrivals/Libs/TempTestCase1560423819123.groovy
@@ -14,7 +14,7 @@ DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.m
 DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
 
 
-RunConfiguration.setExecutionSettingFile('C:\\Users\\user\\AppData\\Local\\Temp\\Katalon\\Test Cases\\virtualShowRSVP\\New Test Case_sushant\\20190613_124218\\execution.properties')
+RunConfiguration.setExecutionSettingFile('C:\\Users\\user\\AppData\\Local\\Temp\\Katalon\\Test Cases\\virtualShowRSVP\\New Test Case_sushant\\20190613_163339\\execution.properties')
 
 TestCaseMain.beforeStart()
 

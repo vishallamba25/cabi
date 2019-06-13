ef5c4571a0581937d0b93d54643554b17114c040
Vishal <vishal.lamba@neosofttech.com>
2019-06-13 17:08:26 +0530

    no message

--- NewArrivals/Scripts/virtualShowRSVP/New Test Case_Vishal/Script1560321705394.groovy
+++ NewArrivals/Scripts/virtualShowRSVP/New Test Case_Vishal/Script1560321705394.groovy
@@ -2,11 +2,16 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
+import org.openqa.selenium.JavascriptExecutor
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.webui.common.WebUiCommonHelper as WebUiCommonHelper
+import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+import internal.GlobalVariable
 
 String dataFile = 'virtualShowData'
 
@@ -53,6 +58,97 @@ listElement.get(2).getText().equalsIgnoreCase(cohostess)
 listElement.get(3).getText().equalsIgnoreCase(guest1)
 
 listElement.get(3).getText().equalsIgnoreCase(guest2)
+
+WebUI.delay(5)
+
+List<WebElement> isShowNotStarted = WebUiCommonHelper.findWebElements(findTestObject('virualShowRSVPOR/dashboard/button_start_pre_show'),
+	5)
+
+if (isShowNotStarted.size() > 0) {
+	WebUI.click(findTestObject('virualShowRSVPOR/dashboard/button_start_pre_show'))
+
+	WebUI.click(findTestObject('virualShowRSVPOR/dashboard/button_pre_show_sure'))
+} else {
+	WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/dashboard/button_re_join_show'))
+}
+
+/////////////////////////
+WebUI.delay(5)
+
+/*WebUI.acceptAlert()
+WebDriver driver = DriverFactory.getWebDriver()
+driver.switchTo().alert().accept();
+*/
+/*****************************tab switching****************/
+String currentPage = WebUI.getUrl()
+
+int currentTab = WebUI.getWindowIndex()
+
+WebDriver driver = DriverFactory.getWebDriver()
+
+JavascriptExecutor js = ((driver) as JavascriptExecutor)
+
+js.executeScript('window.open();')
+
+WebUI.switchToWindowIndex(currentTab + 2)
+
+WebUI.callTestCase(findTestCase('virtualShowRSVP/setVHost'), [:], FailureHandling.STOP_ON_FAILURE)
+
+GlobalVariable.micrositeURL="https://mirandakate.cabitest5.com/show-microsite/104596896/"
+WebUI.navigateToUrl(GlobalVariable.micrositeURL)
+
+WebUI.verifyElementText(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/h1_Youre invited'),
+	'You\'re invited to Front Row')
+
+WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/button_Login to RSVP'))
+
+'Login with the invited guest'
+WebUI.setText(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/input_Sign in  Create account_email'),
+	guest1Mail)
+
+WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/button_Continue'))
+
+WebUI.delay(5)
+
+WebUI.setText(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/input_Welcome_password'),
+	guest1Pass)
+
+WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/button_Continue'))
+
+WebUI.delay(5)
+
+if (WebUI.verifyElementPresent(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/complete_my_profile_later'),
+	0)) {
+	WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/div_Ill complete my profile later' //WebUI.verifyElementPresent(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/div_Update your RSVP'), 0)
+			))
+}
+
+//WebDriver driver = DriverFactory.get DriverFactory.getDriver()
+List<WebElement> listElement1 = WebUiCommonHelper.findWebElements(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/div_Update your RSVP'),
+	5)
+
+'Validating invited guest\'s \'Yes\' RSVP\r\n'
+if (listElement.empty) {
+	println('list is empty')
+
+	println('RSVP is not updated')
+
+	WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/button_RSVP'))
+	WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/select_rsvp_yes'))
+	WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/button_SEND'))
+
+	WebUI.delay(4)
+
+	WebUI.verifyElementText(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/h2_Were so glad you can make it.'),
+		'We\'re so glad you can make it.')
+} else {
+	println('RSVP is updated previously')
+}
+
+
+WebUI.delay(3)
+
+WebUI.switchToWindowIndex(currentTab + 1)
 
 //WebUI.verifyElementText(findTestObject('virualShowRSVPOR/Page_Show microsite/neoGuest1'), guest1)
 

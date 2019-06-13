a7da10723b228538be4b743ede86434919deda11
Vishal <vishal.lamba@neosofttech.com>
2019-06-13 14:23:51 +0530

    no message

--- NewArrivals/Scripts/virtualShowRSVP/New Test Case_Vishal/Script1560321705394.groovy
+++ NewArrivals/Scripts/virtualShowRSVP/New Test Case_Vishal/Script1560321705394.groovy
@@ -2,18 +2,11 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
+import org.openqa.selenium.WebElement
+
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.webui.common.WebUiCommonHelper as WebUiCommonHelper
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-
-WebUI.callTestCase(findTestCase('virtualShowRSVP/createShow'), [('testEnvt') : '', ('username') : '', ('password') : '', ('stylist') : ''
-        , ('hostess') : '', ('cohostess') : '', ('guest1') : '', ('guest2') : '', ('verifyHostess') : '', ('verifyCohostess') : ''
-        , ('verifyGuestCount') : '', ('verifyGuest1') : '', ('verifyGuest2') : '', ('cabiTestEnvt') : ''], FailureHandling.STOP_ON_FAILURE)
-
-WebUI.delay(3)
-
-WebUI.back()
-
-WebUI.click(findTestObject('virualShowRSVPOR/Page_Show microsite/dashboardTab'))
 
 String dataFile = 'virtualShowData'
 
@@ -27,11 +20,39 @@ guest1 = findTestData(dataFile).getValue('guest1', 1)
 
 guest2 = findTestData(dataFile).getValue('guest2', 1)
 
+//WebUI.callTestCase(findTestCase('virtualShowRSVP/createShow'), [('testEnvt') : '', ('username') : '', ('password') : '', ('stylist') : ''
+//        , ('hostess') : '', ('cohostess') : '', ('guest1') : '', ('guest2') : '', ('verifyHostess') : '', ('verifyCohostess') : ''
+//        , ('verifyGuestCount') : '', ('verifyGuest1') : '', ('verifyGuest2') : '', ('cabiTestEnvt') : ''], FailureHandling.STOP_ON_FAILURE)
+WebUI.openBrowser('')
+
+WebUI.maximizeWindow()
+
+WebUI.callTestCase(findTestCase('NewArrival/backOfficeLogin'), [('BOURL') : '', ('BOuser') : '', ('BOpass') : ''], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.navigateToUrl('https://test19.cliotest.com/backoffice/control/VSStylistDashboard?showId=104597108&consultantPartyId=100000042')
+
+WebUI.delay(3)
+
+//WebUI.back()
+
+WebUI.click(findTestObject('virualShowRSVPOR/Page_Show microsite/dashboardTab'))
+
 WebUI.delay(5)
 
 WebUI.switchToFrame(findTestObject('virualShowRSVPOR/dashboard/frame_start_pre_show'), 60)
 
 WebUI.delay(5)
 
-WebUI.verifyElementText(findTestObject('virualShowRSVPOR/Page_Show microsite/neoGuest1'), guest1)
+List<WebElement> listElement = WebUiCommonHelper.findWebElements(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/guestList'),
+	5)
+
+listElement.get(1).getText().equalsIgnoreCase(hostess)
+
+listElement.get(2).getText().equalsIgnoreCase(cohostess)
+
+listElement.get(3).getText().equalsIgnoreCase(guest1)
+
+listElement.get(3).getText().equalsIgnoreCase(guest2)
+
+//WebUI.verifyElementText(findTestObject('virualShowRSVPOR/Page_Show microsite/neoGuest1'), guest1)
 

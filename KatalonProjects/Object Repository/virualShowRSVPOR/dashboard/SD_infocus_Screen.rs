0dbe62f29a36631fe74bac69defc333a6027476b
Aishwarya Pedamkar <aishwarya.pedamkar@neosofttech.com>
2019-06-24 16:08:23 +0530

    Added test cases in VS_RSVP

---
+++ KatalonProjects/Object Repository/virualShowRSVPOR/dashboard/SD_infocus_Screen.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>SD_infocus_Screen</name>
+   <tag></tag>
+   <elementGuidId>0b157f98-8ccd-4ec7-9769-a5bb641eca93</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class = &quot;video-container showVideo useWidth&quot;]//span</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>

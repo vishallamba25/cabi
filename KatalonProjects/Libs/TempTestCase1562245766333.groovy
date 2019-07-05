738e908ca80577e1d32e56576937b59e0093b572
Vishal <vishal.lamba@neosofttech.com>
2019-07-05 10:53:35 +0530

    no message

--- KatalonProjects/Libs/TempTestCase1562245766333.groovy
+++ KatalonProjects/Libs/TempTestCase1562245766333.groovy
@@ -14,9 +14,9 @@ DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.m
 DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
 
 
-RunConfiguration.setExecutionSettingFile('C:\\Users\\user\\AppData\\Local\\Temp\\Katalon\\Test Cases\\TestCaseUtilities\\createContactBO\\20190703_155301\\execution.properties')
+RunConfiguration.setExecutionSettingFile('C:\\Users\\user\\AppData\\Local\\Temp\\Katalon\\Test Cases\\virtualShowRSVP\\New Test Case_sushant\\20190704_183926\\execution.properties')
 
 TestCaseMain.beforeStart()
 
-        TestCaseMain.runTestCase('Test Cases/TestCaseUtilities/createContactBO', new TestCaseBinding('Test Cases/TestCaseUtilities/createContactBO',[:]), FailureHandling.STOP_ON_FAILURE , false)
+        TestCaseMain.runTestCase('Test Cases/virtualShowRSVP/New Test Case_sushant', new TestCaseBinding('Test Cases/virtualShowRSVP/New Test Case_sushant',[:]), FailureHandling.STOP_ON_FAILURE , false)
     

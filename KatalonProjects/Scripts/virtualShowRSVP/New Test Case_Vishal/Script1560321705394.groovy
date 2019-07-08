537d3f46e9b44c7851c9c826b77119f8cfb886d8
vishal_lamba <vishal.lamba@neosofttech.com>
2019-07-08 19:29:33 +0530

    no message

--- KatalonProjects/Scripts/virtualShowRSVP/New Test Case_Vishal/Script1560321705394.groovy
+++ KatalonProjects/Scripts/virtualShowRSVP/New Test Case_Vishal/Script1560321705394.groovy
@@ -1,4 +1,5 @@
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
 import org.openqa.selenium.JavascriptExecutor as JavascriptExecutor
@@ -17,6 +18,8 @@ import internal.GlobalVariable as GlobalVariable
 
 /**********vaiable initialization********************/
 String dataFile2 = "micrositeData"
+hostessMail = findTestData(dataFile2).getValue('hostessMail', 1)
+hostessPass = findTestData(dataFile2).getValue('hostessPass', 1)
 guest1Mail = findTestData(dataFile2).getValue('guest1Mail', 1)
 guest1Pass = findTestData(dataFile2).getValue('guest1Pass', 1)
 /****************************************************/
@@ -28,18 +31,19 @@ WebUI.callTestCase(findTestCase('virtualShowRSVP/createShow'), [('testEnvt') : '
         , ('hostess') : '', ('cohostess') : '', ('guest1') : '', ('guest2') : '', ('verifyHostess') : '', ('verifyCohostess') : ''
         , ('verifyGuestCount') : '', ('verifyGuest1') : '', ('verifyGuest2') : '', ('cabiTestEnvt') : ''], FailureHandling.STOP_ON_FAILURE)
 
-WebUI.openBrowser('')
+
+/*WebUI.openBrowser('')
 WebUI.maximizeWindow()
 WebUI.callTestCase(findTestCase('TestCaseUtilities/backOfficeLogin'), [('BOURL') : '', ('BOuser') : '', ('BOpass') : ''], FailureHandling.STOP_ON_FAILURE)
 WebUI.navigateToUrl('https://test21.cliotest.com/backoffice/control/VSStylistDashboard?showId=104602773&consultantPartyId=100000042')
-
+*/
 WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/Page_cabi Edit Show - Send Invitations/a_dashboard'))
 
 WebUI.delay(5)
 
 WebUI.switchToFrame(findTestObject('virualShowRSVPOR/dashboard/frame_start_pre_show'), 60)
 
-WebUI.delay(5)
+//WebUI.delay(5)
 
 /////////////////////////
 List<WebElement> isShowNotStarted = WebUiCommonHelper.findWebElements(findTestObject('virualShowRSVPOR/dashboard/button_start_pre_show'), 
@@ -47,7 +51,7 @@ List<WebElement> isShowNotStarted = WebUiCommonHelper.findWebElements(findTestOb
 
 if (isShowNotStarted.size() > 0) {
     WebUI.click(findTestObject('virualShowRSVPOR/dashboard/button_start_pre_show'))
-
+	WebUI.delay(3)
     WebUI.click(findTestObject('virualShowRSVPOR/dashboard/button_pre_show_sure'))
 } else {
     WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/dashboard/button_re_join_show'))
@@ -72,8 +76,8 @@ WebUI.switchToWindowIndex(currentTab + 2)
 
 WebUI.callTestCase(findTestCase('TestCaseUtilities/setVHost'), [:], FailureHandling.STOP_ON_FAILURE)
 
-GlobalVariable.micrositeURL="https://mirandakate.cabitest5.com/show-microsite/104602773/"
-WebUI.navigateToUrl(GlobalVariable.micrositeURL)
+//GlobalVariable.micrositeURL="https://mirandakate.cabitest5.com/show-microsite/104602773/"
+/*WebUI.navigateToUrl(GlobalVariable.micrositeURL)
 
 'Login with the invited guest'
 WebUI.setText(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/input_Sign in  Create account_email'),	guest1Mail)
@@ -99,12 +103,33 @@ if (listElement1.empty) {
 } else {
 	println('RSVP is updated previously')
 }
+*/
 
+js.executeScript('window.open();')
 
+WebUI.switchToWindowIndex(currentTab + 3)
+
+WebUI.navigateToUrl(GlobalVariable.micrositeURL)
+
+WebUI.setText(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/input_Sign in  Create account_email'),	hostessMail)
+WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/button_Continue'))
+WebUI.delay(5)
+WebUI.setText(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/input_Welcome_password'), hostessPass)
+WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/button_Continue'))
+WebUI.delay(5)
+if (WebUI.verifyElementPresent(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/complete_my_profile_later'), 0)) {
+	WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/Page_Show microsite/div_Ill complete my profile later'))
+}
+
+/*WebUI.delay(4)
+WebUI.click(findTestObject('Object Repository/showMicrosite/button_join_the_show'))
 WebUI.delay(3)
+WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/dashboard/a_close_mic_alert'))
+
 WebUI.switchToWindowIndex(currentTab + 1)
 WebUI.click(findTestObject('Object Repository/virualShowRSVPOR/dashboard/a_close_mic_alert'))
 WebUI.delay(3)
+*/
 
 
 

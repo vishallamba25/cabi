d557f099546e36a609a6a5bcd6921d8ad8c5bbb2
sushantshinde <sushant.shinde@neosofttech.com>
2019-07-24 14:22:31 +0530

    no message

---
+++ KatalonProjects/Object Repository/virualShowRSVPOR/chatboard/dummy.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>dummy</name>
+   <tag></tag>
+   <elementGuidId>0875a06f-31c4-4ee5-92fa-dc35123fb200</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>

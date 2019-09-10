e3a8b5791a754b5171251e2a7664500b989c19ea
Vishal <vishal.lamba@neosofttech.com>
2019-09-10 12:42:17 +0530

    no message

--- KatalonProjects/Libs/TempTestCase1568013850576.groovy
+++ KatalonProjects/Libs/TempTestCase1568013850576.groovy
@@ -14,7 +14,7 @@ DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.m
 DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
 
 
-RunConfiguration.setExecutionSettingFile('C:\\Users\\user\\AppData\\Local\\Temp\\Katalon\\Test Cases\\VirtualShow\\VS_Automation\\20190906_115200\\execution.properties')
+RunConfiguration.setExecutionSettingFile('C:\\Users\\user\\AppData\\Local\\Temp\\Katalon\\Test Cases\\VirtualShow\\VS_Automation\\20190909_125410\\execution.properties')
 
 TestCaseMain.beforeStart()
 

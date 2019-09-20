df69d8a9df33148d48bc3d8bf0235cada5babd42
Vishal <vishal.lamba@neosofttech.com>
2019-09-20 11:16:21 +0530

    no message

---
+++ KatalonProjects/Object Repository/ReplicatedSite/rsNewUser/editBustSize.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>editBustSize</name>
+   <tag></tag>
+   <elementGuidId>368dc70b-0fec-4bee-83d1-675cd5bf1645</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//span[text()='Bust Sizes:']/parent::div/following-sibling::div//div[@class='drop-caret']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//span[text()='Bust Sizes:']/parent::div/following-sibling::div//div[@class='drop-caret']</value>
+   </webElementProperties>
+</WebElementEntity>
--- KatalonProjects/Scripts/VirtualShow/newUserRegistrationRS/Script1564758359451.groovy
+++ KatalonProjects/Scripts/VirtualShow/newUserRegistrationRS/Script1564758359451.groovy
@@ -10,6 +10,14 @@ import com.kms.katalon.core.webui.common.WebUiCommonHelper as WebUiCommonHelper
 import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import commonUtility.UtilityMethods as UtilityMethods
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import internal.GlobalVariable as GlobalVariable
 
 '/**********Faker initialization for Random Names********************/'
 RSURL = findTestData('envtData').getValue('RSURL', 1)
@@ -69,10 +77,11 @@ WebDriver driver = DriverFactory.getWebDriver()
 
 JavascriptExecutor executor = ((driver) as JavascriptExecutor)
 
-executor.executeScript('arguments[0].click();', WebUiCommonHelper.findWebElement(findTestObject('ReplicatedSite/rsNewUser/gotItThanks'), 
-        5))
+//
+//executor.executeScript('arguments[0].click();', WebUiCommonHelper.findWebElement(findTestObject('ReplicatedSite/rsNewUser/gotItThanks'), 
+//        5))
+WebUI.click(findTestObject('ReplicatedSite/rsNewUser/gotItThanks'))
 
-//WebUI.click(findTestObject('ReplicatedSite/rsNewUser/gotItThanks'))
 //TestObject profileOptions= UtilityMethods.createTestObject("profileOptions", '//div[@class=\'check\' and preceding-sibling::div[text()=\'',Casual,'\']]')
 WebUI.click(findTestObject('ReplicatedSite/rsNewUser/setUpMyProfile'))
 
@@ -115,7 +124,7 @@ WebUI.click(findTestObject('Object Repository/ReplicatedSite/rsNewUser/continueB
 WebUI.delay(5)
 
 '/**********Profile Completion Forms for Size,Types of Dresses etc********************/'
-List<String> titleList = new ArrayList<String>()
+List<String> titleList = new ArrayList()
 
 titleList.add('Bust')
 
@@ -143,7 +152,7 @@ titleList.add('Dresses')
 
 titleList.add('Shoes')
 
-List<String> sizeList = new ArrayList<String>()
+List<String> sizeList = new ArrayList()
 
 sizeList.add('35')
 
@@ -180,7 +189,7 @@ for (int i = 0; i < titleList.size(); i++) {
     WebUI.delay(3)
 }
 
-WebUI.delay(3)
+WebUI.delay(5)
 
 WebUI.click(findTestObject('Object Repository/ReplicatedSite/rsNewUser/continueButtonStyleForm'))
 
@@ -232,4 +241,16 @@ WebUI.click(findTestObject('ReplicatedSite/rsNewUser/dressStyle2'))
 WebUI.click(findTestObject('ReplicatedSite/rsNewUser/saveButton'))
 
 WebUI.verifyElementText(findTestObject('ReplicatedSite/rsNewUser/changesSaved'), 'Your changes have been saved.')
+
+//executor.executeScript('arguments[0].click();', WebUiCommonHelper.findWebElement(findTestObject('ReplicatedSite/rsNewUser/sizeAndMeasurementsTab'), 
+//        5))
+
+//WebUI.click(findTestObject('ReplicatedSite/rsNewUser/sizeAndMeasurementsTab'))
+
+//executor.executeScript('arguments[0].click();', WebUiCommonHelper.findWebElement(findTestObject('ReplicatedSite/rsNewUser/editBustSize'),
+//	5))
+
+//WebUI.click(findTestObject('ReplicatedSite/rsNewUser/editBustSize'))
+
+
 

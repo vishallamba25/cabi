b4cc52703f7bcced697555c2febc0ba873a32586
Vishal <vishal.lamba@neosofttech.com>
2019-10-21 12:39:31 +0530

    no message

--- KatalonProjects/Libs/TempTestCase1571319295676.groovy
+++ KatalonProjects/Libs/TempTestCase1571319295676.groovy
@@ -14,7 +14,7 @@ DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.m
 DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
 
 
-RunConfiguration.setExecutionSettingFile('C:\\Users\\user\\AppData\\Local\\Temp\\Katalon\\Test Cases\\TestCaseUtilities\\createContactBO\\20191017_124217\\execution.properties')
+RunConfiguration.setExecutionSettingFile('C:\\Users\\user\\AppData\\Local\\Temp\\Katalon\\Test Cases\\TestCaseUtilities\\createContactBO\\20191017_190455\\execution.properties')
 
 TestCaseMain.beforeStart()
 
--- KatalonProjects/Test Suites/MassOrderCreationSuite.ts
+++ KatalonProjects/Test Suites/MassOrderCreationSuite.ts
@@ -13,7 +13,7 @@
    <testCaseLink>
       <guid>c7db35e8-0990-41c9-b8aa-9bb55ef05194</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/OrderCreator/addOnOrderPersonal</testCaseId>
       <variableLink>
          <testDataLinkId></testDataLinkId>
@@ -37,25 +37,25 @@
    <testCaseLink>
       <guid>739c675b-7f6b-4f23-858b-b7d329b6eb9a</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/OrderCreator/BOOnline</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>76bd5b38-8ad3-4e7c-b199-52870af9a084</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/OrderCreator/personalOrder</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>d2564939-96c3-4b75-8451-f0376a174d30</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/OrderCreator/retailOrder</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>9932b30a-d596-4886-a560-81298fe39e35</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/OrderCreator/showOrder</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
